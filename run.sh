971f5041d2e1139d75128606219800f935040075
Your Name <you@example.com>
2024-04-22 21:25:41 +0200

    Update run.sh script to set DISPLAY variable correctly and allow root access to X server

--- run.sh
+++ run.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
-export DISPLAY=:0  # Make sure to set the DISPLAY variable correctly
+# Set the DISPLAY variable correctly
+export DISPLAY=:0
 
-xhost +si:localuser:root  # Allow root access to the X server
+# Allow root access to the X server
+xhost +si:localuser:root
 
+# Run the Docker container with appropriate settings
 docker run -it --rm \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
+  -v /etc/machine-id:/etc/machine-id:ro \
+  -v /run/user/$(id -u)/pulse:/run/user/$(id -u)/pulse \
+  -v $HOME/.Xauthority:/root/.Xauthority:ro \
   -e XDG_RUNTIME_DIR=/tmp \
+  --device /dev/dri \
+  --network host \
   yt-video-player
 
-xhost -si:localuser:root  # Remove root access from the X server after you're done
-S
+# Remove root access from the X server after you're done
+xhost -si:localuser:root
