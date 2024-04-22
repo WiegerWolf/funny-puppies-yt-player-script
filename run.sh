412c7d7bc6f648aacc193b867050b2616c6eee22
Your Name <you@example.com>
2024-04-22 21:15:03 +0200

    Add X11 display access to run.sh script

--- run.sh
+++ run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+xhost +local:root  # Allow local root to access X11 display
+
 docker run -it --rm \
   -e DISPLAY=$DISPLAY \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -e XDG_RUNTIME_DIR=/tmp \
   yt-video-player
+
+xhost -local:root  # Restrict X11 access again
